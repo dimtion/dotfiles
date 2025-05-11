5760655279cf9141c9e1aa07eb1c2344b78e3a1b
Loïc Carr <dev@carr.bzh>
2025-05-11 15:32:14 -0700

    Add gitlinker

---
+++ .config/nvim/lua/dimtion/plugins/gitlinker.lua
@@ -0,0 +1,12 @@
+return {
+  "ruifm/gitlinker.nvim",
+  -- enabled = false,
+  keys = {
+    {
+      "<leader>gy",
+      mode = { "n", "v" },
+      desc = "Git repo link",
+    },
+  },
+  opts = { },
+}
