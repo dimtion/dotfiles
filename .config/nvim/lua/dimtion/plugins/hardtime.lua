e39577482d86148ca919f0a9ccda2b657ea62352
Loïc Carr <dev@carr.bzh>
2025-05-11 08:27:14 -0700

    Add hardtime

---
+++ .config/nvim/lua/dimtion/plugins/hardtime.lua
@@ -0,0 +1,16 @@
+return {
+  "m4xshen/hardtime.nvim",
+  -- enabled = false,
+  -- dev = true,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "ibhagwan/fzf-lua",
+  },
+  opts = {
+    restricted_keys = {
+      ["<C-M>"] = {},
+      ["<C-N>"] = {},
+      ["<C-P>"] = {},
+    },
+  },
+}
