752408b953b4cda6aa2f7c03c182749b84a3862f
Loïc Carr <dev@carr.bzh>
2025-05-11 15:41:56 -0700

    Update gitlinker

--- .config/nvim/lua/dimtion/plugins/gitlinker.lua
+++ .config/nvim/lua/dimtion/plugins/gitlinker.lua
@@ -1,12 +1,10 @@
 return {
-  "ruifm/gitlinker.nvim",
+  "linrongbin16/gitlinker.nvim",
   -- enabled = false,
-  keys = {
-    {
-      "<leader>gy",
-      mode = { "n", "v" },
-      desc = "Git repo link",
+  cmd = "GitLink",
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
     },
-  },
-  opts = { },
+    opts = {},
 }
